e612a8f5128d94c9a5d5fcea63d3995da259558f
Egor Postnikov <ea-post134@yandex.ru>
2020-08-09 20:44:56 +0000

    Made loading comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,27 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random fact about me to the page.
- */
-function addRandomFactAboutMe() {
-  const facts = [
-      'Now I am STEP intern in Google', 
-      'I live in Chelyabinsk. It is a big city in Russia', 
-      'I speak Russian(Да!)', 
-      'I study for my bachelor degree in SPbSU',
-      'I love programming',
-      'Обычная русская фраза?! Это что, шутка какая-то?'
-      ];
-
-  // Pick a random fact.
-  const fact = facts[Math.floor(Math.random() * facts.length)];
-
-  // Add it to the page.
-  const factContainer = document.getElementById('fact-container');
-  factContainer.innerText = fact;
-}
-
 function getComments() {
   fetch('/data').then(response => response.json()).then((commentsArray) => {
     let container = document.getElementById('comments-container');
